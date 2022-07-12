38d69196b6b8431f5bd0d8be2c02e7487a5e20a2
Maeve5 <107019969+Maeve5@users.noreply.github.com>
2022-07-12 19:13:15 +0900

    Add files via upload

---
+++ cal copy.js
@@ -0,0 +1,130 @@
+// let numOne = '';
+// let operator = '';
+// let numTwo = '';
+// const result = document.querySelector('#result');
+
+// function onClickNumber(event) {
+//     if(!operator) {
+//         numOne += event.target.textContent;
+//         result.value += event.target.textContent;
+//         return;
+//     }
+
+//     if(!numTwo) {
+//         result.value = '';
+//     }
+
+//     numTwo += event.target.textContent;
+//     result.value += event.target.textContent;
+// }
+
+// document.querySelector("#number").addEventListener('click', onClickNumber);
+
+// ///////
+
+
+// const result = document.getElementById("result");
+// const numbers = document.getElementsByClassName("number");
+// const operators = document.getElementsByClassName("operator");
+// const calculate = document.getElementById("calculate");
+// const clear = document.getElementById("clear");
+
+// let preNum = '';
+// let currentNum = '';
+// let OP = '';
+// let calculating = true;
+
+// function onClickCalculate() {
+//     calculating = false;
+// }
+
+// const onClickClear = () => {
+//     calculating = false;
+//     result.value = '';
+// }
+
+// const onClickNumber = (e) => {
+//     while (calculating) {
+//         if (!OP) {
+//             preNum = e.target.textContent;
+//             result.value += e.target.textContent;
+//             return;
+//         }
+        
+//         if (!currentNum) {
+//             result.value = '';
+//             currentNum =preNum;
+//         }
+//     }
+// }
+
+// const onClickOperator = (e) => {
+//     if (preNum) {
+//         OP = e.target.textContent;
+//         result.value += e.target.textContent;
+//     }
+//     else {
+        
+//     }
+//     return;
+// }
+
+// Array.from(numbers).forEach(number => number.addEventListener("click", onClickNumber));
+// Array.from(operators).forEach(operator => operator.addEventListener("click", onClickOperator));
+
+// if(calculate){
+//     calculate.addEventListener("click", onClickCalculate);
+// }
+
+// if(clear){
+//     clear.addEventListener("click",onClickClear);
+// }
+
+// ///////22자리 계산기
+
+const result = document.getElementById("result");
+const numbers = document.getElementsByClassName("number");
+const operators = document.getElementsByClassName("operator");
+const calculate = document.getElementById("calculate");
+const clear = document.getElementById("clear");
+
+let preNum = '';
+let currentNum = '';
+let OP = '';
+
+function onClickClear() {
+    preNum = '';
+    currentNum = '';
+    OP = '';
+    result.value = '';
+}
+
+function onClickNumber(event) {
+    // if(!OP) {
+        preNum = event.target.textContent;
+        result.value += preNum;
+        preNum = result.value;
+    // }
+
+    // if(!currentNum) {
+    //     result.value = '';
+    // }
+
+    // currentNum = event.target.textContent;
+    // result.value += preNum;
+    // preNum = result.value;
+}
+
+function onClickOperator(event) {
+    if(preNum) {
+        OP = event.target.textContent;
+        
+    }
+}
+
+if(clear){
+    clear.addEventListener("click", onClickClear);
+}
+
+Array.from(numbers).forEach(number => number.addEventListener("click", onClickNumber));
+Array.from(operators).forEach(operator => operator.addEventListener("click", onClickOperator));
